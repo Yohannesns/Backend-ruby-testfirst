12d28c5869b8fad4513789dc3c321262869ec123
yohannesns <yohannesns@gmail.com>
2021-04-21 16:33:29 -0400

    work on lesson5 completed

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+def ftoc(tf)
+  tc = 5.0/9.0 * ( tf - 32 )   
+end  
+
+
+def ctof(tc)
+  tf = (9.0/5.0 * tc )+ 32  
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,25 @@
 #write your code here
+def translate(string)
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  words = string.split
+  new_words = []
+  
+  words.each do |word|
+    new_word = 
+      if word.match(/^.*qu/)
+        first_part = word[/^.*qu/]
+        last_part = word.delete first_part
+        last_part + first_part + 'ay'
+      elsif vowels.include?(word[0])
+        word + 'ay'
+      elsif !vowels.include?(word[0])
+        first_part = word[/[^aeiou]+/]
+        last_part = word.delete first_part
+        last_part + first_part + 'ay'
+      end
+      
+      new_words.push new_word
+    end
+
+      new_words.join(' ')
+end  
